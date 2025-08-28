50979b22bfff1bf296978f40934d816a7832c4ba
HelinSaraBaran <sara501d@edu.zealand.dk>
2025-08-28 21:33:29 +0200

    Add project files.

---
+++ VendingMachineUI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace VendingMachineUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
