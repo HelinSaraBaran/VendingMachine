4454a6ce0c5d887d09c67bd6eeb630e654c408d1
HelinSaraBaran <sara501d@edu.zealand.dk>
2025-08-29 10:33:42 +0200

    properties mm

--- VendingMachine.Domain/Class1.cs
+++ VendingMachine.Domain/Class1.cs
@@ -1,7 +1,31 @@
 ﻿namespace VendingMachine.Domain
 {
-    public class Class1
+
+    // describes our products 
+    public class ProductType
     {
+        // Our properties and get/set (and set is private oop)
+        public int Id {  get; private set; }
+        public string Name { get; private set; }
+        public int BasePrice { get; private set; }
+        public string Category { get; private set; }
+
+        // our constructor 
+        public ProductType(int id, string name, int baseprice, string category)
+        {
+            Id = id;
+            Name = name;
+            BasePrice = baseprice;
+            Category = category;
+
+        }
+        // method 
+        public string Describe()
+        {
+            return Name +" (" + BasePrice + "kr.)";
+        }
+        
+
 
     }
 }
